f3f8c6539bb05c2f8889fd437885b6dd058c4091
MoonLight <M.Pidhornyi1@uni.brighton.ac.uk>
2025-12-22 10:29:23 +0000

    Payment can't be less than 5 pounds exception

--- src/main/java/ci553/happyshop/catalogue/exception/UnderMinimumPaymentException.java
+++ src/main/java/ci553/happyshop/catalogue/exception/UnderMinimumPaymentException.java
@@ -1,5 +1,6 @@
 package ci553.happyshop.catalogue.exception;
 
+//payment can't be less than 5 pounds exception
 public class UnderMinimumPaymentException extends Exception {
     public UnderMinimumPaymentException(String message) {
         super(message);
