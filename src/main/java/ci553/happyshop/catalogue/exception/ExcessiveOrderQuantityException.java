e5b457325caa2cbd2ce01287a1ae01ab67e4b638
MoonLight <M.Pidhornyi1@uni.brighton.ac.uk>
2025-12-22 10:28:00 +0000

    Number of products must be < 50 exception

--- src/main/java/ci553/happyshop/catalogue/exception/ExcessiveOrderQuantityException.java
+++ src/main/java/ci553/happyshop/catalogue/exception/ExcessiveOrderQuantityException.java
@@ -1,5 +1,6 @@
 package ci553.happyshop.catalogue.exception;
 
+// number of products must be < 50 exception
 public class ExcessiveOrderQuantityException extends Exception {
     public ExcessiveOrderQuantityException(String message) {
         super(message);
