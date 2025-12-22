4c2899ede5508fb1802958699ded5670fe4eab19
MoonLight <M.Pidhornyi1@uni.brighton.ac.uk>
2025-12-22 10:31:41 +0000

    now supports multiple search results & name searches

--- src/main/java/ci553/happyshop/client/customer/CustomerView.java
+++ src/main/java/ci553/happyshop/client/customer/CustomerView.java
@@ -194,6 +194,7 @@ public class CustomerView  {
 
     // now supports multiple search results & name searches
 
+
     public void update(String imageName, String searchResult, String trolley, String receipt) {
 
         //  if multi-search is detected, hide image & only show text
